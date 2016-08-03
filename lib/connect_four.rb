ee172267b9a8c7012a2db1aa73bf4ef57932c484
jerrykuo7727 <jerrykuo7727@gmail.com>
2016-08-03 14:51:38 +0800

    Build test of #ask_column

---
+++ lib/connect_four.rb
@@ -0,0 +1,15 @@
+class ConnectFour
+  def initialize
+    @turns = ['R', 'Y']
+    @turn = 0
+    @board = Array.new(6, Array.new(7, '.'))
+  end
+
+  def ask_column
+    col = nil
+    until ["1", "2", "3", "4", "5", "6", "7"].include?(col) do
+      col = gets.chomp
+    end
+    col.to_i
+  end
+end
--- spec/connect_four_spec.rb
+++ spec/connect_four_spec.rb
@@ -1,1 +1,26 @@
 require 'connect_four'
+
+describe ConnectFour do
+  let(:connect_four) { ConnectFour.new }
+
+  describe '#ask_column' do
+    context 'asking user which column to drop disk' do
+      it 'returns the column user input' do
+        io_obj = double
+        allow(connect_four).to receive(:gets).and_return(io_obj)
+        allow(io_obj).to receive(:chomp).and_return("7")
+        expect(connect_four.ask_column).to eql(7)
+      end
+      it 'asks user input again if invalid input' do
+        io_obj = double
+        allow(connect_four).to receive(:gets).and_return(io_obj)
+        allow(io_obj).to receive(:chomp).and_return("A")
+        allow(connect_four).to receive(:gets).and_return(io_obj)
+        allow(io_obj).to receive(:chomp).and_return("8")
+        allow(connect_four).to receive(:gets).and_return(io_obj)
+        allow(io_obj).to receive(:chomp).and_return("4")
+        expect(connect_four.ask_column).to eql(4)
+      end
+    end
+  end
+end
